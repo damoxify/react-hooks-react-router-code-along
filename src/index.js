94ef3488c60f2217dc22da5e8a9e38ef353cab55
Adedamola Ajasa <azeemajasa@gmail.com>
2023-09-10 08:51:02 +0100

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,16 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./App";
+// Step 1. Import react-router functions
+import { BrowserRouter } from "react-router-dom";
 
-function Home() {
-  return (
-    <div>
-      <h1>Home!</h1>
-    </div>
-  );
-}
 
-function App() {
-  return <Home />;
-}
-
-ReactDOM.render(<App />, document.getElementById("root"));
+// Step 3. Use <BrowserRouter> component to wrap the entire application and provide React Router context features
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById("root")
+);
